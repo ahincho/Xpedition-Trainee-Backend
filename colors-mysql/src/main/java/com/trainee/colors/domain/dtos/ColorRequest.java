c7371c4030e86932812d3a4589ff8e2a05a84c67
ahincho <ahincho@unsa.edu.pe>
2023-10-22 01:47:39 -0500

    Changing Default Messages on Validator

--- colors-mysql/src/main/java/com/trainee/colors/domain/dtos/ColorRequest.java
+++ colors-mysql/src/main/java/com/trainee/colors/domain/dtos/ColorRequest.java
@@ -4,12 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record ColorRequest (
-        @NotBlank String name,
-        @NotBlank
-        @Pattern(regexp = "^#[A-F0-9]{6}$")
+        @NotBlank(message = "Name must not be blank")
+        String name,
+        @NotBlank(message = "Color must not be blank")
+        @Pattern(regexp = "^#[A-F0-9]{6}$", message = "Color should be a valid hexadecimal format like #A1B2C3")
         String color,
-        @NotBlank
-        @Pattern(regexp = "^[0-9]{2}-[0-9]{4}$")
+        @NotBlank(message = "Pantone must not be blank")
+        @Pattern(regexp = "^[0-9]{2}-[0-9]{4}$", message = "Pantone should follow the format 'dd-dddd'")
         String pantone
 ) {
 
